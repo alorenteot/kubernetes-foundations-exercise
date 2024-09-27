91ebac0ecf41d4ae1382549e73f20f91ee1e38ec
Jose Torró <46130387+JoseCarTorBel@users.noreply.github.com>
2024-09-27 14:08:53 +0200

    added database

--- kubernetes/init.sh
+++ kubernetes/init.sh
@@ -4,5 +4,8 @@ minikube start
 kubectl config set-context --current --namespace=dev
 kubectl apply -f deployers/
 kubectl apply -f services/
-kubectl apply -d CronJob.yaml
+kubectl apply -f CronJob.yaml
+kubectl apply -f configmaps/
+kubectl apply -f secrets/
+kubectl apply -f statefulset/
 minikube dashboard
